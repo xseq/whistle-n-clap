2a3d6654bfca2f145613fccdb57d0d1749792bb8
hillLang <xuan.zhong06@gmail.com>
2022-07-31 10:33:13 -0700

    small files only

---
+++ training.py
@@ -0,0 +1,115 @@
+# This is a simple realization of CNN based on this article: https://zhuanlan.zhihu.com/p/52298361
+# The key: six steps of building a machine learning application
+# 0) Proprocessing
+# 1) Define inputs and outputs
+# 2) Modeling
+# 3) Define the cost function
+# 4) Define the optimization method
+# 5) Train and validate the model
+# 6) Check the results
+
+# environment: (spoken_numbers) python 3.9.7, tensorflow 2.8.0
+# notes for tf 2.8 https://www.tensorflow.org/tutorials/quickstart/beginner
+
+
+import os
+import logging
+import tensorflow as tf
+
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.system('clear')
+tf.random.set_seed(111)
+
+
+# Step 0: Feature preprocessing, needed for audio but not for images
+mnist = tf.keras.datasets.mnist
+
+
+# Step 1: Shapes of inputs and outputs
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+input_shape = x_train.shape[1] * x_train.shape[2]  # row * column
+output_shape = y_test.max() - y_train.min() + 1    # 10
+print("Input Shape: " + str(input_shape))
+print("Output Shape: " + str(output_shape))
+
+
+# Step 2: Modeling
+model = tf.keras.models.Sequential([    
+
+    # conv1
+    tf.keras.layers.Conv2D(
+        data_format='channels_last',
+        input_shape=(28, 28, 1),
+        filters=32,
+        kernel_size=(5, 5),
+        padding='same',
+        activation=tf.nn.relu),  # output size: [n_samples,28,28,32]
+
+    # maxpooling1
+    tf.keras.layers.MaxPool2D(
+        pool_size=(2, 2),
+        strides=2,
+        padding='same'),  # output size: [n_samples,14,14,32]
+
+    # fc1
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(
+        units=256, 
+        activation=tf.nn.relu
+    ),
+
+    # dropout 1
+    tf.keras.layers.Dropout(
+        rate=0.3
+    ),
+
+    # fc2
+    tf.keras.layers.Dense(
+        units=32, 
+        activation=tf.nn.relu
+    ),
+
+    # dropout 2
+    tf.keras.layers.Dropout(
+        rate=0.3
+    ),
+
+    # fc3
+    tf.keras.layers.Dense(
+        units=10, 
+        activation=tf.nn.relu
+    )
+
+])
+
+
+# Step 3: Define a loss function
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+# loss_fn = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+
+
+# Step 4: Optimization method
+model.compile(optimizer='adam',
+    loss=loss_fn,
+    metrics=['accuracy'])
+
+
+# Step 5: Training
+model.fit(x_train, y_train, epochs=50)
+
+
+# Step 6: Evaluation
+model.evaluate(x_test,  y_test)
+
+
+# probability_model = tf.keras.Sequential([
+#     model,
+#     tf.keras.layers.Softmax()
+# ])
+# print(probability_model(x_test[:5]))
+
+
--- util/build_data_set.py
+++ util/build_data_set.py
@@ -1,4 +1,4 @@
-# copying a subset of audio data from the dataset folder to a local folder
+# copying a subset of FSD50k audio data from the dataset folder to a local folder
 # use conda environment spoken_numbers
 
 import numpy as np
@@ -10,6 +10,9 @@ import shutil
 os.system('clear')
 proj_path = os.path.abspath(os.getcwd())
 
+# parameters
+MAX_WAV_FILE_SIZE = 500000
+
 # change the following to the source path name
 wav_src_path = '.../Dataset/FSD50k/FSD50K.dev_audio/'
 wav_dst_path = proj_path + '/data/wav/'
@@ -19,16 +22,41 @@ with open(proj_path + '/csv/categories.csv', newline='') as csvfile:
 with open(proj_path + '/csv/dev.csv', newline='') as csvfile:
     dev_list = np.array(list(csv.reader(csvfile)))
 
+n_categories = len(categories)
 n_dev_data = len(dev_list)
-n_wav_files = 0
+file_count = [0] * n_categories
 
-for p in range(n_dev_data):
-    if dev_list[p, 1] in categories: 
-        n_wav_files += 1
-        src_name = wav_src_path + str(dev_list[p, 0]) + '.wav'
-        dst_name = wav_dst_path + str(dev_list[p, 0]) + '.wav'
-        print('Copying file: '+ str(dev_list[p, 0]) + '.wav')
-        shutil.copy(src_name, dst_name)
+# moving files
+for p in range(1, n_dev_data):
+    category_name = dev_list[p, 1]
+    src_name = wav_src_path + str(dev_list[p, 0]) + '.wav'
+    file_size = os.path.getsize(src_name)
+    if file_size < MAX_WAV_FILE_SIZE:
+        for q in range(n_categories):
+            if category_name == categories[q, 0]:
+                file_count[q] += 1
+                dst_name = wav_dst_path + str(dev_list[p, 0]) + '.wav'
+                print('Copying file of category ' + 
+                    categories[q, 1] + ': ' + str(dev_list[p, 0]) + '.wav')
+                shutil.copy(src_name, dst_name)
 
-print('The number of wav files copied: ' + str(n_wav_files))
+# display results
+print(' ')
+print('Task complete.')
+for p in range(n_categories):
+    print(categories[p, 1] + ': ' + str(file_count[p]))
+print('The number of wav files copied: ' + str(sum(file_count)))
+
+# results
+# Chink and Clink: 83
+# Clapping Hands: 293
+# Dropping Coins: 289
+# Coughing: 168
+# Opening Drawer: 66
+# Snapping Fingers: 126
+# Jangling Keys: 86
+# Knocking Doors: 35
+# Laughing: 516
+# Walking: 150
+# The number of wav files copied: 1812
 
