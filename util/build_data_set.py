6b4fffbae6d4a4e63dac67d227320b2fb2e0c375
hillLang <xuan.zhong06@gmail.com>
2022-07-30 17:37:21 -0700

    creating data set

--- util/build_data_set.py
+++ util/build_data_set.py
@@ -4,12 +4,28 @@
 import numpy as np
 import csv
 import os
+import shutil
 
 
 os.system('clear')
-path = os.path.abspath(os.getcwd())
+proj_path = os.path.abspath(os.getcwd())
+wav_src_path = '/media/xuan/XZ/Dataset/FSD50k/FSD50K.dev_audio/'
+wav_dst_path = proj_path + '/data/wav/'
 
-with open(path + '/csv/categories.csv', newline='') as csvfile:
-    categories = list(csv.reader(csvfile))
+with open(proj_path + '/csv/categories.csv', newline='') as csvfile:
+    categories = np.array(list(csv.reader(csvfile)))
+with open(proj_path + '/csv/dev.csv', newline='') as csvfile:
+    dev_list = np.array(list(csv.reader(csvfile)))
 
-print(categories[0][1])
+n_dev_data = len(dev_list)
+n_wav_files = 0
+
+for p in range(n_dev_data):
+    if dev_list[p, 1] in categories: 
+        n_wav_files += 1
+        src_name = wav_src_path + str(dev_list[p, 0]) + '.wav'
+        dst_name = wav_dst_path + str(dev_list[p, 0]) + '.wav'
+        print('Copying file: '+ str(dev_list[p, 0]) + '.wav')
+        shutil.copy(src_name, dst_name)
+
+print('The number of wav files copied: ' + str(n_wav_files))
