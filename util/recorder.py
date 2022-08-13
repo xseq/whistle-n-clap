468548aaa7020d2ecd525551d73c7111540c8e0b
hillLang <xuan.zhong06@gmail.com>
2022-08-12 18:38:49 -0700

    recorder working

--- util/recorder.py
+++ util/recorder.py
@@ -1,3 +1,5 @@
+# record audio for a limited period of time
+
 import pyaudio
 import wave
 import numpy as np
@@ -28,66 +30,47 @@ device_idx = []
 for p in range(len(device_list)):
     if device_name in device_list[p]['name']:
         device_idx = p
-print(device_idx)
 
 
-frame_size = 1024
+frame_size = 256
 sample_format = pyaudio.paInt16
 n_channels = 1
 FS = 44100
-filename = "output.wav"
+FILE_NAME = "temp.wav"
 CLIP_DURATION = 3   # seconds
-MAX_DURATION = 60   # seconds
-
-
-# load model
-proj_path = os.path.abspath(os.getcwd())
-f_name = proj_path + '/models/cnn_20220802.h5'
-model = load_model(f_name)
-model.summary()
-print('Model Loaded!')
 
 
 audio_obj = pyaudio.PyAudio()  # portaudio interface
 print('Recording')
 
 stream = audio_obj.open(format=sample_format,
-                channels=1,
+                channels=n_channels,
                 rate=FS,
                 frames_per_buffer=frame_size,
-                input_device_index=2,
+                input_device_index=device_idx,
                 input=True)
 
 
-n_buffer_samples = MAX_DURATION * FS
-buffer = [0.0] * n_buffer_samples  # Initialize queue
-
-
-plt.figure(1)
-# Store data in chunks for 3 seconds
-for p in range(0, int(FS / frame_size * MAX_DURATION)):
-    frame_data = np.frombuffer(stream.read(frame_size),dtype=np.float32)
-    # frame = np.array(stream.read(frame_size))
-    # frame_data = frame.astype(np.float32, order='C') / 32768.0
-    buffer = np.append(buffer[len(frame_data):], frame_data)
-    features = get_features(buffer, FS)
-    y_pred = model.predict(features)
-    print('Prediction: '  + str(y_pred))
-
+record_frames = []
+stream.start_stream()
+for p in range(0, int(FS / frame_size * CLIP_DURATION)):
+    data = stream.read(frame_size)
+    record_frames.append(data)
 
 
 # Stop and close the stream 
 stream.stop_stream()
 stream.close()
 # Terminate the PortAudio interface
-p.terminate()
+audio_obj.terminate()
 
+
+# Save the recorded data as a WAV file
+wf = wave.open(FILE_NAME, 'wb')
+wf.setnchannels(n_channels)
+wf.setsampwidth(audio_obj.get_sample_size(sample_format))
+wf.setframerate(FS)
+wf.writeframes(b''.join(record_frames))
+wf.close()
 print('Finished recording')
 
-# # Save the recorded data as a WAV file
-# wf = wave.open(filename, 'wb')
-# wf.setnchannels(n_channels)
-# wf.setsampwidth(p.get_sample_size(sample_format))
-# wf.setframerate(FS)
-# wf.writeframes(b''.join(frames))
-# wf.close()
